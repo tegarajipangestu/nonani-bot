d6a6560b70f0d1a5b3247a131cb8b3d86221aa6a
tegarajipangestu <tegarajipangestu@gmail.com>
2017-02-06 20:52:01 +0700

    Initial commit, mvp version

---
+++ nonanibot.rb
@@ -0,0 +1,35 @@
+require 'telegram/bot'
+require 'csv'
+
+token = '323650569:AAH0miXDpgJJQoFOJ9Mr2HQ8QMLP281Iq1w'
+
+def search_from_file(tag)
+  result = []
+  @jokes.each do |row|
+    result << row if row['tags'] == tag
+  end
+  result
+end
+
+def load_from_file
+  @jokes ||= begin
+    jokes = []
+    jokes_file = 'nonanibot-db - jokebapakbapak.csv'
+    CSV.foreach(jokes_file, headers: true) do |row|
+      jokes << row
+    end
+    jokes
+  end
+end
+
+Telegram::Bot::Client.run(token) do |bot|
+  puts "Starting bot"
+  load_from_file
+  bot.listen do |message|
+    jokes = search_from_file(message.text)
+    unless jokes.empty?
+      n = rand(0..jokes.size)
+      bot.api.send_message(chat_id: message.chat.id, text: jokes[n]['pun'].to_s)
+    end
+  end
+end
